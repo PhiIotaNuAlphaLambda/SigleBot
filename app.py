3903ac945270edf0328afc107d7778e9037d7924
Devin Mork <devin.mork@gmail.com>
2022-07-17 14:56:20 -0500

    Modified app.py.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from decouple import config
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
   return "Hello World!"
+
+# query for new tweets that follow particular pattern.
