6a5157fb5af453891dab8f569e4e6d025b4a6b06
Devin Mork <devin.mork@gmail.com>
2022-07-19 16:56:51 -0500

    Add logic to delete tweets older than 14 days, clean up old print commands, add fresh print commands.

--- twitter.py
+++ twitter.py
@@ -2,7 +2,7 @@ from decouple import config
 import tweepy
 import re
 from app import RetweetedTweet, db
-from datetime import datetime
+from datetime import datetime, timedelta
 import pprint
 
 pp = pprint.PrettyPrinter()
@@ -89,7 +89,6 @@ def deduplicate_tweets(new_tweets_list, model):
   Compares list up tweets against recently tweeted links stored in database, removing them from the list of tweets that are to be retweeted.
   """
   new_tweets_list = deduplicate_own_list(new_tweets_list)
-
   new_tweets_list = deduplicate_in_db(new_tweets_list, model)
   return new_tweets_list
 
@@ -101,18 +100,14 @@ def deduplicate_in_db(new_tweets_list, model):
     db_pulled_urls.append(db_tweet.post_url)
 
   deduplicated_tweets = []
-  print(type(new_tweets_list[0].post_url[0]))
   for tweet in new_tweets_list:
     if tweet.post_url[0] not in db_pulled_urls:
-      print(f"starting length {len(deduplicated_tweets)}")
       deduplicated_tweets.append(tweet)
-      print(f"ending length {len(deduplicated_tweets)}")
 
   return deduplicated_tweets
 
 
 def deduplicate_own_list(new_tweets_list):
-  print(f"Original length {len(new_tweets_list)}")
   deduplicated_urls = []
 
   local_deduplicated_tweets = []
@@ -122,7 +117,6 @@ def deduplicate_own_list(new_tweets_list):
       local_deduplicated_tweets.append(tweet)
       deduplicated_urls.append(tweet.post_url[0])
 
-  print(f"Ending length {len(local_deduplicated_tweets)}")  
   return local_deduplicated_tweets
 
 
@@ -130,12 +124,13 @@ def log_retweeted_urls(successfully_retweeted_urls):
   """
   Record tweet into the DB to ensure it doesn't get tweeted again anytime soon.
   """
+  count = 0
   for post_url in successfully_retweeted_urls:
     adding_tweet = RetweetedTweet(post_url=post_url, date_retweeted=datetime.now())
     db.session.add(adding_tweet)
-    db.session.commit()
-    print('tweet committed')
-  
+    count += 1
+  db.session.commit()
+  print(f"Committed {count} tweets to the db.")
   return True
 
 
@@ -149,7 +144,20 @@ def send_retweets(cleaned_tweets_list, tweepy_client):
       # print(f"Retweeted & Archived Tweet ID {tweet.tweet_id} with link to {tweet.post_url[0]}")
     except:
       continue
+  print(f"Retweeted {len(successfully_retweeted_urls)} Sigle stories.")
   return successfully_retweeted_urls
+
+def remove_older_tweets(days_old, model, db):
+  count = 0
+  old_datetime = datetime.now() - timedelta(days=days_old)
+  all_tweets = model.query.all()
+  for tweet in all_tweets:
+    if tweet.date_retweeted <= old_datetime:
+      count += 1
+      db.session.delete(tweet)
+  db.session.commit()
+  print(f"Deleted {count} old tweets from the database.")
+  return True
 
 
 def activate_siglebot():
@@ -159,8 +167,11 @@ def activate_siglebot():
   tweepy_client = initialize_tweepy_client()
   downloaded_tweets = query_twitter(tweepy_client)
   downloaded_tweets = organize_tweets(downloaded_tweets)
+  print(f"Twitter URL response looks like: {downloaded_tweets[0].post_url[0]}")
   downloaded_tweets = deduplicate_tweets(downloaded_tweets, RetweetedTweet)
   sent_urls = send_retweets(downloaded_tweets, tweepy_client)
   log_retweeted_urls(sent_urls)
+  remove_older_tweets(14, RetweetedTweet, db)
   return True
-  
+
+activate_siglebot()
