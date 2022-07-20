65f8cd2f480c8c2a2da9947f8b34f1d231b53750
Devin Mork <devin.mork@gmail.com>
2022-07-20 10:09:54 -0500

    Removed old code.

--- old_code/new_tweet.py
+++
@@ -1,9 +0,0 @@
-﻿class NewTweet:
-  
-  def __init__(self, author_id, tweet_id, post_url, tweet_datetime):
-    self.author_id = author_id
-    self.tweet_id = tweet_id
-    self.post_url = post_url,
-    self.tweet_datetime = tweet_datetime
-
-
